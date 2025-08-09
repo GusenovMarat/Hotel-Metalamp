fcb49b22dd05b338db08bbb0228e3ce0f462da25
Gusenov Marat <gusenovmaratr@gmail.com>
2025-08-09 12:47:56 +0300

    delete initStyles function in checkbox.js

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -20,30 +20,15 @@ class CheckboxList {
 			const expand = item.querySelector(this.checkboxExpand);
 			const block = item.querySelector(this.checkboxBlock);
 			const toggle = item.querySelector(this.checkboxToggle);
-			this.initStyles(block);
-
+			block.style.maxHeight = '0px'
 			expand.addEventListener('click', () => this.toggleHandler(block, toggle));
 		});
 	}
 
-	initStyles(element) {
-		if (element) {
-				Object.assign(element.style, {
-				maxHeight: '0',
-				overflow: 'hidden',
-				transition: 'max-height 0.3s ease',
-				display: 'flex'
-			})
-		}
-	}
-	
 	toggleHandler (block, toggle) {
 		const isHidden = block.style.maxHeight === '0px';
-		
 		block.style.maxHeight = (isHidden) ? `${block.scrollHeight}px` 
 		: '0px';
-
-		toggle.style.transition = 'transform 0.3s ease';
     toggle.style.transform = isHidden ? 'rotate(180deg)' : 'rotate(0)'
 		
 	}
