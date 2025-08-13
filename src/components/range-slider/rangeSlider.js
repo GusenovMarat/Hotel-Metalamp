901f7f2fa91470bbbdfe616d902dd04881678595
Gusenov Marat <gusenovmaratr@gmail.com>
2025-08-13 16:00:35 +0300

    add func updataRange in rangeSlider.js

--- src/components/range-slider/rangeSlider.js
+++ src/components/range-slider/rangeSlider.js
@@ -17,10 +17,28 @@ class RangeSlider {
 
 	init() {
 		this.rangeSlider.forEach(item => {
-			
+			const priceMin = item.querySelector(`${this.RSPrice}[data-limit="min"]`);
+			const priceMax = item.querySelector(`${this.RSPrice}[data-limit="max"]`);
+			const rangeProgress = item.querySelector(this.RSProgress);
+   		const rangeValueMin = item.querySelector(`${this.RSValue}[data-value="min"]`);
+   		const rangeValueMax = item.querySelector(`${this.RSValue}[data-value="max"]`);
+			this.updateRange(rangeValueMin, rangeValueMax, priceMin, priceMax, rangeProgress);
 		})
 	}
 
+	updateRange = (valueMin, valueMax, priceMin, priceMax, progress) => {
+    const minVal = parseInt(valueMin.value);
+    const maxVal = parseInt(valueMax.value);
+
+    priceMin.textContent = minVal + "₽";
+    priceMax.textContent = maxVal + "₽";
+
+    const progressLeft = ((minVal / valueMin.max) * 100) + "%";
+    const progressRight = 100 - ((maxVal / valueMax.max) * 100) + "%";
+		
+    progress.style.left = progressLeft;
+    progress.style.right = progressRight;
+   };
 }
 
 export default RangeSlider
