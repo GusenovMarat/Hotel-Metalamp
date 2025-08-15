0a96e0af46675677a59dd301fedd3fb50a8ba0b4
Gusenov Marat <gusenovmaratr@gmail.com>
2025-08-15 16:29:10 +0300

    end stylisation calendar date-picker

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -3,7 +3,10 @@ import DatePicker from '@/libs/datepicker';
 document.addEventListener("DOMContentLoaded", () => {
 		const options = {
 			nextHtml: 'arrow_forward',
-			prevHtml: 'arrow_forward'
+			prevHtml: 'arrow_back',
+			navTitles: {
+        days: 'MMMM yyyy'
+    	}
 		}
     const datePicker = new DatePicker(options);
     datePicker.init();
