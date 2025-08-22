771f5f1bae389ca13bb87588973dc0d4754a2389
Gusenov Marat <gusenovmaratr@gmail.com>
2025-08-22 22:50:05 +0300

    checkboxlist changed to toggle classlist

--- src/components/checkbox/checkboxList.js
+++ src/components/checkbox/checkboxList.js
@@ -8,7 +8,9 @@ class CheckboxList {
 		this.checkboxSelector = '.js-checkbox';
 		this.checkboxExpand = '.js-checkbox-expand';
 		this.checkboxBlock = '.js-checkbox-block';
-		this.checkboxToggle = '.js-checkbox-toggle'
+		this.checkboxToggle = '.js-checkbox-toggle';
+		this.checkboxActive = 'checkbox__block-active';
+		this.checkboxActiveToggle = 'checkbox__toggle-active'
 	}
 
 	findCheckbox() {
@@ -17,20 +19,15 @@ class CheckboxList {
 
 	init() {
 		this.checkbox.forEach(item => {
-			const block = item.querySelector(this.checkboxBlock);
 			const expand = item.querySelector(this.checkboxExpand);
-			const toggle = item.querySelector(this.checkboxToggle);
-
-			block.style.maxHeight = '0px'
-			expand.addEventListener('click', () => this.toggleHandler(block, toggle));
+			const block = item.querySelector(this.checkboxBlock);
+			const toggleIcon = item.querySelector(this.checkboxToggle);
+			
+			expand.addEventListener('click', () => {
+					block.classList.toggle(this.checkboxActive);	
+					toggleIcon.classList.toggle(this.checkboxActiveToggle);		
+			});
 		});
-	}
-
-	toggleHandler (block, toggle) {
-		const isHidden = block.style.maxHeight === '0px';
-		block.style.maxHeight = isHidden ? `${block.scrollHeight}px` : '0px';
-    toggle.style.transform = isHidden ? 'rotate(180deg)' : 'rotate(0)'
-		
 	}
 };
 
