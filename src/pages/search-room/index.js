8737b6647cb9e9debe84043f5154b4471698e2d0
Gusenov Marat <gusenovmaratr@gmail.com>
2025-08-25 10:43:58 +0300

    add sidebar json in search-room page

--- src/pages/search-room/index.js
+++ src/pages/search-room/index.js
@@ -1,1 +1,11 @@
 import './search-room.scss'
+
+// import '@/components/rate-button';
+
+import '@/components/dropdown';
+
+import '@/components/date-field';
+
+import '@/components/range-slider';
+
+import '@/components/checkbox';
